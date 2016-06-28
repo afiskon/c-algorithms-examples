89e8b9f5b6e4797e4791b3f6860e51493a130663
Aleksander Alekseev <afiskon@gmail.com>
2016-06-28 16:26:10 +0300

    dlist_example.c created

---
+++ dlist_example.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "deps/algorithms/include/ilist.h"
+
+typedef struct
+{
+	dlist_node node;
+	char data[128];
+} ListItemData;
+
+typedef ListItemData *ListItem;
+
+void main()
+{
+	dlist_iter iter;
+	ListItemData item1, item2, item3;
+	ListItem tmp;
+
+	dlist_head head;
+	dlist_init(&head);
+
+	strcpy(item1.data, "first item");
+	strcpy(item2.data, "second item");
+	strcpy(item3.data, "third item");
+
+	printf("is empty before: %d\n", dlist_is_empty(&head));
+
+	dlist_push_head(&head, (dlist_node*) &item1);
+	dlist_push_head(&head, (dlist_node*) &item2);
+	dlist_push_head(&head, (dlist_node*) &item3);
+
+	printf("is empty aftre: %d\n", dlist_is_empty(&head));
+
+	dlist_foreach(iter, &head)
+	{
+		tmp = (ListItem) iter.cur;
+		printf("tmp->data = %s\n", tmp->data);
+	}
+
+	tmp = (ListItem) dlist_pop_head_node(&head);
+	printf("After dlist_pop_head_node call tmp->data = %s\n", tmp->data);
+
+	dlist_foreach(iter, &head)
+	{
+		tmp = (ListItem) iter.cur;
+		printf("tmp->data = %s\n", tmp->data);
+	}
+}
